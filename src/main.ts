253f88666f420cb8341c1092b081592e19eb8b21
liguiqinxxn <1043736808@qq.com>
2023-05-07 16:11:02 +0800

    引入common.less

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,14 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import ElementPlus from "element-plus";
+import "element-plus/dist/index.css";
+import "@/assets/less/common.less";
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.use(store)
-app.use(router)
-app.use(ElementPlus)
-app.mount('#app')
+app.use(store);
+app.use(router);
+app.use(ElementPlus);
+app.mount("#app");
